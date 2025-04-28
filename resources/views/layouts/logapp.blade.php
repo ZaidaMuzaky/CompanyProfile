50fee983feffd6e923d11ad9f2c6589cb71f3f9f
Zaida Muzaky <711muzaky@gmail.com>
2025-04-28 17:10:18 +0700

    naming update

--- resources/views/layouts/logapp.blade.php
+++ resources/views/layouts/logapp.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'MyApp')</title>
+    <title>@yield('title', 'Departemen Plant Site KDA')</title>
 
     <!-- Favicons -->
     <link href="{{ asset('assets/img/rzlogo.png') }}" rel="icon">
