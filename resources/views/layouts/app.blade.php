d5a17f00c7eea0b2c0caffb104e49b93ff112082
Zaida Muzaky <711muzaky@gmail.com>
2025-04-28 16:56:23 +0700

    update header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <title>@yield('title', 'FlexStart - Laravel')</title>
+    <title>@yield('title', 'Departemen Plant Site KDA')</title>
     <meta name="description" content="">
     <meta name="keywords" content="">
     <meta name="csrf-token" content="{{ csrf_token() }}">
