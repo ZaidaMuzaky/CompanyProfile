6e287d11f2692729a70f53da170d2f0bcf2e1276
Zaida Muzaky <711muzaky@gmail.com>
2025-10-06 12:17:56 +0700

    update menu

--- resources/views/partials/navbarapp.blade.php
+++ resources/views/partials/navbarapp.blade.php
@@ -121,7 +121,7 @@
                 </li> --}}
 
                 <!-- KPI PLANT -->
-                {{-- @if ($menus->count() > 0)
+                @if ($menus->count() > 0)
                     <li>
                         <a class="nav-link collapsed" data-bs-target="#menu-database" data-bs-toggle="collapse"
                             href="#">
@@ -141,7 +141,7 @@
                             @endforeach
                         </ul>
                     </li>
-                @endif --}}
+                @endif
 
                 <!-- AUDIT SERVICE -->
                 @if ($audits->count() > 0)
@@ -303,13 +303,13 @@
                             <i class="bi bi-newspaper"></i> <span>Manajemen Berita</span>
                         </a>
                     </li>
-
+                    {{-- 
                     <li>
                         <a class="nav-link {{ request()->routeIs('admin.pareto.*') ? 'active' : 'collapsed' }}"
                             href="{{ route('admin.pareto.index') }}">
                             <i class="bi bi-diagram-3"></i> <span>Pareto Problem Unit</span>
                         </a>
-                    </li>
+                    </li> --}}
 
                     <li>
                         <a class="nav-link {{ request()->routeIs('admin.audit.*') ? 'active' : 'collapsed' }}"
